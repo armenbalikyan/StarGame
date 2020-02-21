689bc224be8a17ab3372e5fab1c840f2250fea7e
armenbalikyan <armbalikyans@gmail.com>
2020-02-21 13:02:59 +0400

    Updates

---
+++ precache-manifest.48ae64f28a7cd3b2548159a20229fc1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54438c5e7f5d2ec0344b45f26644955",
+    "url": "/StarGame/index.html"
+  },
+  {
+    "revision": "245749ac73c9db9a4afc",
+    "url": "/StarGame/static/css/main.9ff26ffb.chunk.css"
+  },
+  {
+    "revision": "b3f9bb124c59e85644ba",
+    "url": "/StarGame/static/js/2.61c2377a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/StarGame/static/js/2.61c2377a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245749ac73c9db9a4afc",
+    "url": "/StarGame/static/js/main.bd369414.chunk.js"
+  },
+  {
+    "revision": "ab5143b5f0df304c076e",
+    "url": "/StarGame/static/js/runtime-main.39f2f863.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/StarGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
